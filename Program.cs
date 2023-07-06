01ff8d5b6415c3cd32c308b8e3f87ffd9d35c13d
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-07-06 15:43:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿var result = fibonacci(5);
+Console.WriteLine(result);
+
+int fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
